ab3ec0ae974a44bd63b4486bc01876adebf5061e
abibi189 <bibi1482@mylaurier.ca>
2023-11-10 13:24:32 -0500

    add lab5.html

---
+++ lab5.js
@@ -0,0 +1,6 @@
+class CenterHtml extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>`;
+    }
+}
+customElements.define("x-center", CenterHtml);
